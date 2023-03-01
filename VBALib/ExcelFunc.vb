202849b191bf238116ecdbc8108b2407dd63f3f7
DonZpp <41147561+DonZpp@users.noreply.github.com>
2023-03-01 19:46:44 +0800

    a better way to iterate the cell within specific words

--- VBALib/ExcelFunc.vb
+++ VBALib/ExcelFunc.vb
@@ -1,4 +1,4 @@
-' 将数字转换为26进制，且符号为A-Z
+' convert the number to the cell columns name
 Function Num2Col(n As Integer)
     Dim res As Integer
     res = n Mod 26
@@ -7,12 +7,25 @@ Function Num2Col(n As Integer)
     i = i + res
     Dim c As String
     c = Chr(i)
-    Dim sPre As String
-    sPre = ""
+    Dim strPre As String
+    strPre = ""
     If (n >= 26) Then
         n = (n - 26) \ 26
-        sPre = Num2Col(n)
+        strPre = Num2Col(n)
     End If
-    sPre = sPre & c
-    Num2Col = sPre
+    strPre = strPre & c
+    Num2Col = strPre
+End Function
+
+' locate cells within pointed words
+Function CellsLocate(sUsedRange As Range, strName As String, sCellPos() As Long)
+    Dim pos As Integer
+    pos = 0
+    For Each Cell In sUsedRange.Cells
+        If InStr(Cell.Value, strName) <> 0 Then
+            sCellPos(pos * 2) = Cell.Column
+            sCellPos(pos * 2 + 1) = Cell.Row
+            pos = pos + 1
+        End If
+    Next
 End Function
--- VBALib/SomethingForNothing.vb
+++ VBALib/SomethingForNothing.vb
@@ -1,26 +1,9 @@
 Sub ScheduleGenerator()
-    Dim sName As String
-    sName = "??????"
-    Dim i As Integer
-    Dim j As Integer
-    i = 0
-    While (i < 75)
-        j = 4
-        Dim sCol As String
-        Dim tmp As Integer
-        tmp = i
-        sCol = Num2Col(tmp)
-        While (j < 24)
-            Dim sCellName As String
-            sCellName = sCol & CStr(j)
-            Dim sCell As String
-            sCell = Range(sCellName).Value
-            If InStr(sCell, sName) <> 0 Then
-                Range(sCellName).Value = "test"
-            End If
-            j = j + 1
-        Wend
-        i = i + 1
-
-    Wend
+    Dim strName As String
+    strName = "ÔøÝ¼»Ô"
+    Dim sCells(100) As Variant
+    Call CellsLocate(ActiveSheet.UsedRange, strName, sCells)
+    For Each sCellPos In sCells
+        MsgBox sCellPos
+    Next
 End Sub
